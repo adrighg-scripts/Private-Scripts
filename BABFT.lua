2385040e5fc2d5dd198a15678631f4eb1e7aad1a
adrighg-scripts <nadineadrian2012.nb@myyahoo.com>
2026-02-19 01:49:45 +0100

    Update and rename BABFT.lua to Build a Boat for a Treasure

--- BABFT.lua
+++
@@ -1,6 +0,0 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/catblox1346/StensUIReMake/refs/heads/main/Script/boatbuilderhub_B1"),true))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hassanxzayn-lua/Anti-afk/main/antiafkbyhassanxzyn"))();
